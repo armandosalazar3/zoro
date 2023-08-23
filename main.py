8f89dfcc46c94f625adfc9c79666df3870284876
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-08-24 01:20:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import aiofiles
 from pyrogram.types import User, Message
 # import progressor
 # from progressor import progress_for_pyrogram
-#import sys
+import sys
 import re
 import os
 import io
