5fec85b474d647b118f35f81ee1c31b6a63235fc
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-08-27 23:25:47 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 9541960
 api_hash = "17a1aa509b35564481dae98d89d0191d"
-bot_token = "6317224996:AAG-fIs-iPGfaVmHmePIPspVYMgVpSTBAYA"
+bot_token = "6610143533:AAFvYZMQtcJeFenFlOAVTRP1pMo8F_f47HI"
 
